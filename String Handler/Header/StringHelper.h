37e1f4266189b67ea3e1f326ab66e59bc658e308
neozerahan <157869886+neozerahan@users.noreply.github.com>
2024-01-27 15:48:17 +0530

    Delete String Handler directory
    Made a mistake by uploading it to the wrong repo

--- String Handler/Header/StringHelper.h
+++
@@ -1,8 +0,0 @@
-#define FALSE 0
-#define TRUE 1
-
-void CopyWord(char * word01, const char * word02);
-int CalculateWordLength(const char * word);
-void ClearBuffer();
-int CompareWords(const char * word01, const char * word02); 
-char * ConnectWords(const char * word01, const char * word02);
